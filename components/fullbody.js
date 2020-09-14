a60dbc832d608a3d29af532c225329a14eb56a74
ronan <ronan@thenextweb.com>
2020-09-14 22:59:19 +0200

    update fullbody for home codin

---
+++ components/fullbody.js
@@ -0,0 +1,222 @@
+<html lang="en"><head>
+
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+
+	<link href="https://cdn.jsdelivr.net/npm/tailwindcss/dist/tailwind.min.css" rel="stylesheet">
+
+	<title>Nextwind</title>
+
+</head>
+<body class="font-sans font-thin">
+
+	<div class="bg-black fixed top-0 left-0 w-full h-full z-50 flex items-center justify-center" style="display: none;">
+		<span class="absolute top-0 right-0 block w-24 h-24 flex items-center justify-center text-white font-black text-white text-2xl mr-6 mt-6 cursor-pointer">close</span>
+		<ul class="flex flex-col text-center text-white font-black text-lg md:text-4xl leading-loose tracking-wider uppercase">
+			<li><a href="/" class="block hover:opacity-50">Home</a></li>
+			<li><a href="/what-we-do/branding" class="block hover:opacity-50">Branding</a></li>
+			<li><a href="/what-we-do/websites" class="block hover:opacity-50">Website Design</a></li>
+			<li><a href="/what-we-do/advertising" class="block hover:opacity-50">Advertising</a></li>
+			<li><a href="/work-with-us" class="block hover:opacity-50">Contact</a></li>
+			<li><a href="/blog" class="block hover:opacity-50">Blog</a></li>
+		</ul>
+	</div>
+	
+	<div class="relative z-10 w-full h-24 px-12 bg-white flex justify-between">
+		<a href="#" class="block h-full w-12 flex items-center text-lg md:text-2xl font-bold tracking-widest text-black uppercase hover:text-black">Nextwind</a>
+		<a href="#" class="relative block px-3 h-24 bg-black text-white text-center tracking-widest uppercase text-xs font-bold pt-10 pb-6 flex flex-col items-center justify-between hover:bg-gray-900">
+			<span>menu</span>
+		</a>
+	</div>
+
+	<!-- HEADER / -->
+
+
+	<div class="py-24 px-12 md:px-32">
+		<div class="container mx-auto">
+			<h1 class="text-2xl md:text-6xl font-black uppercase text-black tracking-widest leading-normal mb-12 pl-10">SINGULAR ARTISTS</h1>
+			<div class="font-serif leading-loose tracking-wide text-base md:text-lg text-black opacity-50 max-w-xl md:ml-64">
+				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque vel venenatis velit. Donec placerat, quam eu fermentum vestibulum, justo felis accumsan metus, eget mollis est nibh in sapien. </p>
+			</div>
+		</div>
+	</div>
+	
+	<!-- / HEADER -->
+
+	<div class="bg-gray-200 py-24 relative">
+		<div class="bottom-auto top-0 left-0 right-0 w-full absolute pointer-events-none overflow-hidden -mt-20" style="height:80px;transform:translateZ(0)">
+			<svg class="absolute bottom-0 overflow-hidden" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="none" version="1.1" viewBox="0 0 2560 100" x="0" y="0">
+				<polygon class="text-gray-200 fill-current" points="2560 0 2560 100 0 100"></polygon>
+			</svg>
+		</div>
+		<div class="w-full h-full top-0 left-0 absolute flex flex-col items-center justify-start"></div>
+		<div class="container mx-auto px-12">
+			<h2 class="text-black uppercase font-bold tracking-widest text-3xl pb-4">Just Announced</h2>
+			<div class="relative z-10 w-full flex flex-wrap">
+				
+				<div class="md:mb-2 md:mb-0 w-full md:w-2/3 h-64 md:h-96 bg-black overflow-hidden relative">
+					<img src="https://images.unsplash.com/photo-1545060894-7b45236d00b3?w=1200" alt="Citybrand Website Design by Digizu" class="w-full h-auto opacity-50">
+					<h2 class="text-white uppercase font-bold tracking-widest text-3xl absolute bottom-0 left-0 pl-10 pb-10">City Brand</h2>
+				</div>
+				<div class="flex-1 h-64 md:h-96 flex flex-wrap md:pl-1">
+					<div class="w-1/2 h-1/2 md:px-1 md:pb-1">
+						<img src="https://images.unsplash.com/photo-1545060894-7b45236d00b3?w=1200" alt="" class="px-6 bg-white w-full h-full object-contain filter-grayscale">
+					</div>
+					<div class="w-1/2 h-1/2 md:px-1 md:pb-1">
+						<img src="https://images.unsplash.com/photo-1545060894-7b45236d00b3?w=1200" alt="" class="w-full h-full object-cover filter-grayscale">
+						<h2 class="text-black">City Brand</h2>
+					</div>
+				</div>
+			</div>
+			<div class="relative z-10 w-full flex flex-wrap">
+				<div class="w-full md:flex-1 h-64 md:h-96 flex flex-wrap md:pr-1">
+					<div class="w-1/2 h-1/2 md:px-1 md:pb-1">
+						<img src="https://placedog.net/1200/350" alt="" class="w-full h-full object-cover filter-grayscale">
+					</div>
+					<div class="w-1/2 h-1/2 md:px-1 md:pb-1">
+						<img src="https://placedog.net/1200/350" alt="TEST UNIT Branding Design by Digizu" class="w-full h-full object-cover filter-grayscale">
+					</div>
+				</div>
+				<div class="w-full md:w-2/3 h-64 md:h-96 bg-black overflow-hidden relative">
+					<img src="https://placedog.net/1200/350" alt="TEST UNIT Design by Digizu" class="w-full h-auto opacity-50">
+					<h2 class="text-white text-right uppercase font-bold tracking-widest text-3xl absolute bottom-0 right-0 pr-10 pb-10">TEST UNIT</h2>
+				</div>
+			</div>
+		</div>
+		<div class="relative mt-24 -mb-24 bg-black px-12 py-24 flex items-center">
+			<div class="hidden md:block w-3/12"></div>
+			<div class="flex-1 relative">
+				<div class="font-serif text-white opacity-75 text-base md:text-xl max-w-2xl leading-loose">
+					<h2 class="text-white uppercase font-bold tracking-widest text-3xl pb-4">About Me</h2>
+					<p>Branding is the foundation of your business. A fantastic digital experience relies on the brand resonating perfectly with your target audience. We help businesses assert their position in their industry by providing a brand identity that's solid and memorable.</p>
+				</div>
+				<span class="absolute top-0 left-0 w-4 h-full bg-white opacity-50 -ml-20"></span>
+			</div>
+		</div>
+	</div>
+	<div class="w-full h-screen bg-white flex flex-wrap">	
+		<div class="px-12 md:-px-0 w-full md:w-4/12 bg-white flex items-center justify-start md:justify-end">
+			<div class="shadow-xl md:shadow-none px-12 py-12 md:pl-36 md:pr-24 bg-white md:-mr-64 relative z-10">
+				<h1 class="mb-8 text-3xl md:text-5xl text-black text-bold uppercase font-bold tracking-widest leading-tight">
+					LATEST TOUR ANNOUNCEMENTS
+				</h1>
+				<h2 class="text-lg md:text-2xl font-serif text-gray-500">Exceptional Contemporary Living</h2>
+			</div>
+		</div>
+		<div class="-mt-64 md:mt-0 w-full md:flex-1">
+			<img src="https://images.unsplash.com/photo-1566981731417-d4c8e17a9e82?crop=entropy&amp;cs=tinysrgb&amp;fit=crop&amp;fm=jpg&amp;h=900&amp;ixid=eyJhcHBfaWQiOjF9&amp;ixlib=rb-1.2.1&amp;q=80&amp;w=800" class="w-full h-full object-cover">
+		</div>
+	</div>
+
+	<div class="bg-black px-12 py-24 flex items-center">
+		<div class="hidden md:block w-3/12"></div>
+		<div class="flex-1 relative">
+			<h2 class="mb-8 uppercase text-white text-2xl font-bold tracking-widest">Our Expertise</h2>
+			<p class="font-serif text-gray-500 text-base md:text-xl max-w-2xl leading-loose">RJ Investments brings luxury living to the rental market through bespoke renovations, at affordable prices with quality property management.</p>
+			<span class="absolute top-0 left-0 w-4 h-full bg-gray-300 -ml-20"></span>
+		</div>
+	</div>
+
+	<!-- SPACER / -->
+	<div class="w-full h-24 border-black bg-white flex justify-end">
+		<div class="w-64 bg-black"></div>
+	</div>
+	<!-- / SPACER -->
+
+	<div class="w-full flex items-center justify-start relative">
+		<div class="w-auto md:w-3/12"></div>
+		<div class="w-full md:w-8/12 bg-gray-100 py-12 relative flex">
+			<a href="#" class="block relative z-10 bg-white h-full w-full px-12 md:px-0 md:w-6/12 py-24 md:-ml-24">
+				<h3 class="mb-2 uppercase text-gray-800 text-3xl font-bold tracking-widest">Shows</h3>
+				<p class="text-lg md:text-xl font-serif text-gray-400">Completed Builds</p>
+			</a>
+			<img src="https://images.unsplash.com/photo-1566981731417-d4c8e17a9e82?crop=entropy&amp;cs=tinysrgb&amp;fit=crop&amp;fm=jpg&amp;h=900&amp;ixid=eyJhcHBfaWQiOjF9&amp;ixlib=rb-1.2.1&amp;q=80&amp;w=1200" class="absolute top-0 left-0 block w-full h-full object-cover">
+		</div>
+		<div class="w-1/12 absolute top-0 right-0 h-full bg-black"></div>
+	</div>
+
+	<!-- SPACER / -->
+	<div class="w-full h-12 border-black bg-white flex justify-end">
+		<div class="w-64 bg-black"></div>
+	</div>
+	<!-- / SPACER -->
+
+	<div class="w-full flex items-center justify-start relative">
+		<div class="w-auto md:w-3/12"></div>
+		<div class="w-full md:w-8/12 bg-gray-100 py-12 relative flex">
+			<a href="#" class="block relative z-10 bg-white h-full w-full px-12 md:px-0 md:w-6/12 py-24 md:-ml-24">
+				<h3 class="mb-2 uppercase text-gray-800 text-3xl font-bold tracking-widest">Artists</h3>
+				<p class="text-lg md:text-xl font-serif text-gray-400">Current Developments</p>
+			</a>
+			<img src="https://images.unsplash.com/photo-1566981731417-d4c8e17a9e82?crop=entropy&amp;cs=tinysrgb&amp;fit=crop&amp;fm=jpg&amp;h=900&amp;ixid=eyJhcHBfaWQiOjF9&amp;ixlib=rb-1.2.1&amp;q=80&amp;w=1200" class="absolute top-0 left-0 block w-full h-full object-cover">
+		</div>
+
+		<div class="w-1/12 absolute top-0 right-0 h-full bg-black"></div>
+	</div>
+
+
+	<!-- SPACER / -->
+	<div class="w-full h-12 border-black bg-white flex justify-end">
+		<div class="w-64 bg-black"></div>
+	</div>
+	<!-- / SPACER -->
+
+
+	<div class="w-full flex items-center justify-start relative">
+		<div class="w-1/12 absolute top-0 right-0 h-full bg-black"></div>
+	</div>
+
+
+	<!-- SPACER / -->
+	<div class="w-full h-24 border-black bg-white flex justify-end">
+		<div class="w-64 bg-black"></div>
+	</div>
+	<!-- / SPACER -->
+
+	<div class="relative bg-black px-12 py-24 flex items-center">
+		<div class="hidden md:block w-3/12"></div>
+		<div class="relative flex flex-col min-w-0 break-words md:w-1/2 mb-6 shadow-lg rounded-lg bg-gray-300">
+			<div class="flex-auto p-5 lg:p-10">
+				<form>
+					<h4 class="text-2xl font-semibold">Want to get in touch with us?</h4>
+					<p class="leading-relaxed mt-1 mb-4 text-gray-600">Complete this form and we will get back to you in 24 hours.</p>
+					<div class="relative w-full mb-3 mt-8">
+						<label class="block uppercase text-gray-700 text-xs font-bold mb-2" for="full-name">Full Name</label>
+						<input name="fullname" class="px-3 py-3 placeholder-gray-400 text-gray-700 bg-white rounded text-sm shadow focus:outline-none focus:shadow-outline w-full" placeholder="Full Name" style="transition: all 0.15s ease 0s;">
+					</div>
+					<div class="relative w-full mb-3">
+						<label class="block uppercase text-gray-700 text-xs font-bold mb-2" for="email">Email</label>
+						<input type="email" name="email" class="px-3 py-3 placeholder-gray-400 text-gray-700 bg-white rounded text-sm shadow focus:outline-none focus:shadow-outline w-full" placeholder="Enter Your Email Address" style="transition: all 0.15s ease 0s;">
+					</div>
+					
+					<div class="relative w-full mb-3">
+						<label class="block uppercase text-gray-700 text-xs font-bold mb-2" for="message">Message</label>
+						<textarea rows="4" cols="80" class="px-3 py-3 placeholder-gray-400 text-gray-700 bg-white rounded text-sm shadow focus:outline-none focus:shadow-outline w-full" placeholder="Type a message..." name="message"></textarea>
+					</div>
+					
+					<div class="text-center mt-6">
+						<input class="bg-gray-900 text-white active:bg-gray-700 text-sm font-bold uppercase px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1" type="submit">
+					</div>
+				</form>
+			</div>
+		</div> 
+	</div>
+	
+	<footer class="relative bg-gray-300 pt-8 pb-6">
+		<div class="bottom-auto top-0 left-0 right-0 w-full absolute pointer-events-none overflow-hidden -mt-20" style="height:80px;transform:translateZ(0)">
+			<svg class="absolute bottom-0 overflow-hidden" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="none" version="1.1" viewBox="0 0 2560 100" x="0" y="0">
+				<polygon class="text-gray-300 fill-current" points="2560 0 2560 100 0 100"></polygon>
+			</svg>
+		</div>
+		<div class="container mx-auto px-4">
+			<div class="flex flex-wrap items-center md:justify-between justify-center">
+				<div class="w-full md:w-4/12 px-4 mx-auto text-center">
+					<div class="text-sm text-gray-600 font-semibold py-1">Copyright © <!-- -->2020<!-- --> <!-- -->NextWind by<!-- --> <a href="https://ronan-oleary.com" class="text-gray-600 hover:text-gray-900">Ro</a>.
+					</div>
+				</div>
+			</div>
+		</div>
+	</footer>
+	<!-- / FOOTER -->
+</body></html>
