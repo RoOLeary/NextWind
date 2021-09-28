ea9b4ecc605fb2a591aa2b72c6e249295700f86d
ronan <ronan@thenextweb.com>
2021-09-28 12:50:59 +0200

    update the blurb a bit

--- components/fullbody.js
+++ components/fullbody.js
@@ -28,7 +28,7 @@ const Fullbody = () => {
 				<div className="flex">
 					<img class="starting-logo dark-version default-logo" alt="Ronan O'Leary" src="https://ronan-oleary.com/wp-content/uploads/2017/06/rosenberg.png" style={{ "height": "150px" }} />
 					<div className="font-serif leading-loose tracking-wide text-base md:text-lg text-black opacity-50 max-w-lg md:ml-12">
-						<p>Versatile, experienced Web Developer and architect, with 12 years professional experience, predominantly in front-end engineering, but not shy of working with back-end languages, technologies and configurations.</p>
+						<p>I'm a versatile, experienced Web Developer and Producer, with 12 years professional experience crafting websites and applications, predominantly rooted in front-end engineering, but not shy of working with back-end languages, technologies and configurations.</p>
 					</div>
 				</div>
 			</div>
