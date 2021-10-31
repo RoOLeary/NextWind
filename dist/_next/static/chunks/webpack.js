0b2da9d40422922e5720efbeb3b0321c426f1979
Ronan <roleary81@gmail.com>
2021-10-31 15:58:09 +0100

    update

--- dist/_next/static/chunks/webpack.js
+++ dist/_next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "7537bdf60d0183870fe8";
+/******/ 	var hotCurrentHash = "0ce7ab225ad817cb5900";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
