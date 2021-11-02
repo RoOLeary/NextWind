9e1b0fe5dc21094ab5822f9289393c581abfdd1d
Ronan <roleary81@gmail.com>
2021-11-02 08:36:13 +0100

    some transitions

--- pages/_app.js
+++ pages/_app.js
@@ -3,7 +3,7 @@ import Head from 'next/head'
 import { ModalContextProvider } from './../contexts/modalContext';
 import AOS from 'aos';
 import 'aos/dist/aos.css';
-import { motion } from 'framer-motion';
+import { AnimatePresence, motion } from 'framer-motion';
 
 function NextWind({ Component, pageProps }) {
   return (
@@ -12,22 +12,24 @@ function NextWind({ Component, pageProps }) {
           <title>Ronan O'Leary | Web Developer</title>
           <meta name="viewport" content="initial-scale=1.0, width=device-width" />
         </Head>
-        <motion.div
-          initial="initial"
-          animate="animate"
-          variants={{
-            initial: {
-              opacity: 0,
-            },
-            animate: {
-              opacity: 1,
-            },
-          }}
-        >
-        <ModalContextProvider>
-          <Component {...pageProps} />
-        </ModalContextProvider>
-        </motion.div>
+        <AnimatePresence exitBeforeEnter>
+          <motion.div
+            initial="initial"
+            animate="animate"
+            variants={{
+              initial: {
+                opacity: 0,
+              },
+              animate: {
+                opacity: 1,
+              },
+            }}
+          >
+          <ModalContextProvider>
+            <Component {...pageProps} />
+          </ModalContextProvider>
+          </motion.div>
+        </AnimatePresence>
       </>
     )
 }
