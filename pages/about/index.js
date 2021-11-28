36c434f63f2c4aea9305d283334b1c67dfe794db
ronan <ronan@thenextweb.com>
2021-11-28 15:34:57 +0100

    that will do for now

--- pages/about/index.js
+++ pages/about/index.js
@@ -34,9 +34,7 @@ export default function About() {
             </div>
             <h1 className="text-xl font-semibold text-gray-700 mt-8 dark:text-gray-200">Social Links</h1>
             <div className="mt-4 ml-4">
-              <div className="flex flex-row justify-start items-center ">
-                  <a href="https://facebook.com/" className="flex flex-row items-center space-x-4 group"><div className="my-4">→</div><p className="text-lg text-gray-500 font-mono relative overflow-hidden dark:text-gray-300"><div className="absolute h-0.5 w-full bg-gray-400 bottom-0 transform -translate-x-24 group-hover:translate-x-0 transition duration-300"></div>Facebook</p></a>
-                </div>
+              
                 <div className="flex flex-row justify-start items-center">
                   <a href="https://twitter.com/ro_oleary" className="flex flex-row items-center space-x-4 group"><div className="my-4">→</div><p className="text-lg text-gray-500 font-mono relative overflow-hidden dark:text-gray-300"><div className="absolute h-0.5 w-full bg-gray-400 bottom-0 transform -translate-x-24 group-hover:translate-x-0 transition duration-300"></div>Twitter</p></a>
                 </div>
