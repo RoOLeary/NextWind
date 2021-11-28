6b99960174a8cc583905bdf9b5cdef3763ecf285
ronan <ronan@thenextweb.com>
2021-11-28 15:35:56 +0100

    see it in print

--- components/homeintro.js
+++ components/homeintro.js
@@ -7,7 +7,7 @@ export default function HomeIntro() {
 				<div className="flex">
 					<img className="starting-logo dark-version default-logo" alt="Ronan O'Leary" src="https://ronan-oleary.com/wp-content/uploads/2017/06/rosenberg.png" style={{ "height": "150px" }} />
 					<div className="font-serif leading-loose tracking-wide text-base md:text-lg text-black opacity-50 max-w-lg md:ml-12">
-						<p>I'm a versatile, experienced Web Developer and Producer, with 12 years professional experience crafting websites and applications, predominantly rooted in front-end engineering, but not shy of working with back-end languages, technologies and configurations.</p>
+						<p>I'm a versatile, experienced Web Developer, Web Product owner and currently CTO at The Next Web, with 12 years professional experience crafting websites and applications, predominantly rooted in front-end engineering, but not shy of working with back-end languages, technologies and configurations.</p>
 					</div>
 				</div>
 			</div>
